40b70221a22445cd90104d4fce28019f2813ea10
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-18 21:15:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,9 +94,6 @@ def adding_matz
         }
      }
      
-    programmer_hash[:yukihiro_matsumoto] => "{
-      :known_for => "Ruby",
-      :languages => ["LISP", "C"]" 
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
