b3a1d08b1f42cfe9a6f2af85b5e2074376b4c21a
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-18 21:02:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,6 +60,22 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+  	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     return programmer_hash[:dennis_ritchie][:languages] = "C"
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
